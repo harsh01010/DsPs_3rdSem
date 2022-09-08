6a2dbb1fff0d0e19ac4f3e6a89c7add67e9acac0
Atul <atulharsh274@gmail.com>
2022-09-08 17:28:49 +0530

    added weekday

--- cs_5.cpp
+++ cs_5.cpp
@@ -1,33 +1,45 @@
-#include <bits/stdc++.h>
-#define ll long long
+#include<bits/stdc++.h>
+typedef long long int ll;
 using namespace std;
 
-int main()
-{
+ void dayy(void)
+ {
+       /*
+           1/1/2000 -> refrence
+            saturday
+         */
+        // for current century
+        // for including other centuries also , check all the year which are divisible by
+         // 4 but not divisible by 400 , other things will be same as below
+        int day , month , yr; cin>>day>>month>>yr;
+        int total=0; yr-=2000; // change ref year here
 
-    int d;
-    cin >> d;
-    switch (d)
-    {
-    case 1:
-        cout << "monday\n";
-        break;
-    case 2:
-        cout << "tuesday\n";
-    case 3:
-        cout << "wed\n";
-        break;
-    case 4:
-        cout << "thurs\n";
-        break;
-    case 5:
-        cout << "fri\n";
-        break;
-    case 6:
-        cout << "sat\n";
-    case 7:
-        cout << "sun\n";
-    }
 
-    return 0;
+  int lp = (yr/4 + 1);
+  int nlp = (yr - yr/4-1);
+         total = (lp*366) + (nlp*365);
+        total += day; month--;
+          //cout<<total<<"\n";
+       
+        int daysOfMth[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+        if( !(yr%4)) daysOfMth[1] = 29;
+
+        for(int i=0;i<month;i++) total+=daysOfMth[i];
+        //cout<<total<<"\n";      
+       string arr[] = {"friday","saturday","sunday","monday","tuesday","wednesday","thursday"};
+       total%=7;
+        cout<<arr[total]<<"\n";
+ }   
+     int main()
+{   
+     
+     /* #ifndef ONLINE_JUDGE
+     freopen("i_p.txt", "r", stdin);
+     freopen("o_p.txt", "w", stdout);
+     #endif
+   */
+       
+     dayy();
+
+return 0;
 }
