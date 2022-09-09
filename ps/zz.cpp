b9569556038c5708e5ae3534fade2e18f7ad7a82
Atul <atulharsh274@gmail.com>
2022-09-09 19:12:58 +0530

    added

---
+++ ps/zz.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+typedef long long int ll;
+using namespace std;
+
+
+void pat1(void)
+{ 
+      for(int i=1;i<=4;i++)
+      {   int p = i;
+          for(int j=1;j<=i;j++)
+          {
+              cout<<p++;
+          }
+          cout<<"\n";
+      }
+}
+
+void pat1(void)
+{ 
+      for(int i=1;i<=4;i++)
+      {   int p = i;
+          for(int j=1;j<=i;j++)
+          {
+              cout<<p++<<" ";
+          }
+          cout<<"\n";
+      }
+}
+
+void pat1(void)
+{ 
+      for(int i=1;i<=4;i++)
+      {   int p = i;
+          for(int j=1;j<=i;j++)
+          {
+              cout<<p++<<" ";
+          }
+          cout<<"\n";
+      }
+}
+
+void pat2(void)
+{
+      
+}
+
+void pat2(void)
+{
+      
+}
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("i_p.txt", "r", stdin);
+    freopen("o_p.txt", "w", stdout);
+    #endif
+
+     pat1();
+
+
+
+return 0;
+}
